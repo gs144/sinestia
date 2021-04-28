caddd945a7964d2b326447fb5662169070baf230
Apolo846 <artur.apolo@hotmail.com>
2021-04-28 15:32:28 -0300

    aa

--- Sinestia/Assets/Scripts/ControleUI.cs
+++ Sinestia/Assets/Scripts/ControleUI.cs
@@ -12,10 +12,7 @@ public class ControleUI : MonoBehaviour
     public AudioMixer mixer;
     public string cena;
     public GameObject tela;
-    void Start()
-    {
-
-    }
+    public AudioSource Som;
 
     public void SfxChange()
     {
@@ -35,28 +32,46 @@ public class ControleUI : MonoBehaviour
     }
     public void ChangeScene()
     {
+        Som.Play();
         SceneManager.LoadScene(cena);
         if(Time.timeScale == 0)
         {
             Time.timeScale = 1;
         }
+
     }
     public void Open()
     {
         tela.SetActive(true);
+        Som.Play();
     }
     public void Close()
     {
-        tela.SetActive(false);
+
+        Som.Play();
+        StartCoroutine(tempo());
     }
     public void Pausa()
     {
         tela.SetActive(true);
         Time.timeScale = 0;
+        Som.Play();
     }
     public void DesPausa()
     {
-        tela.SetActive(false);
+        Som.Play();
+        Invoke("Fechar", 0.1f);
         Time.timeScale = 1;
     }
+    void Fechar()
+    {
+        tela.SetActive(false);
+    }
+    IEnumerator tempo()
+    {
+
+        yield return new WaitForSecondsRealtime(0.1f);
+        tela.SetActive(false);
+    }
+
 }
