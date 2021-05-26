aec8c716bec37240e6c636e937516a8439c3376e
gs144 <otaviobiavateoliveira@hotmail.com>
2021-05-26 15:35:20 -0300

    Bipo Idle

---
+++ Sinestia/Assets/Scripts/Player/PlayIdle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayIdle : MonoBehaviour
+{
+    public  Animator anim;
+    
+    void Start()
+    {
+        anim.SetBool("Idle", true);
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
