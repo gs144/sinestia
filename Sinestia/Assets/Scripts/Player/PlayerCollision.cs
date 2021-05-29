d28d8d5fe6d20998d0c0791ee50fab7c629afeb1
gs144 <otaviobiavateoliveira@hotmail.com>
2021-05-29 17:21:57 -0300

    Ajustes RigidBody Bipo

---
+++ Sinestia/Assets/Scripts/Player/PlayerCollision.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+
+    /*public AudioSource audios;
+    public AudioClip audio_vida, audio_escudo, audio_perderVida, audio_Slide, audio_Pulo;
+
+    void OnTriggerEnter(Collider outro)
+    {
+        if (outro.gameObject.CompareTag("Vida"))
+        {
+            audios.PlayOneShot(audio_vida, 1.0f);
+            Vida++;
+
+
+        }
+        if (outro.gameObject.CompareTag("Escudo"))
+        {
+            audios.PlayOneShot(audio_escudo, 1.0f);
+            shield = true;
+            Escudo.SetActive(true);
+            Invoke("DesativaEscudo", 10.0f);
+        }
+        if (outro.gameObject.CompareTag("Obstaculo"))
+        {
+            if (shield == true)
+            {
+                Vida = Vida;
+            }
+            else
+            {
+                Vida--;
+                audios.PlayOneShot(audio_perderVida, 1.0f);
+            }
+        }
+    }*/
+}
