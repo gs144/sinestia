3ff845a74a285ae88cfefe06d7e1b6de21f840a1
gs144 <otaviobiavateoliveira@hotmail.com>
2021-05-30 17:10:35 -0300

    Cinamatica
    Cena com video passam para o tutorial

---
+++ Sinestia/Assets/Scripts/Controladores/Cinematic.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class Cinematic : MonoBehaviour
+{
+    VideoPlayer video;
+
+    void Awake()
+    {
+        video = GetComponent<VideoPlayer>();
+        video.Play();
+        video.loopPointReached += CheckOver;
+
+
+    }
+
+
+    void CheckOver(UnityEngine.Video.VideoPlayer vp)
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+
+}
