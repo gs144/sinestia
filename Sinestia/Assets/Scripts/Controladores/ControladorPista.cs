887c7d74a15b64158852eaa9d4661813ec1040f0
gs144 <otaviobiavateoliveira@hotmail.com>
2021-06-10 16:47:28 -0300

    Bug pista

--- Sinestia/Assets/Scripts/Controladores/ControladorPista.cs
+++ Sinestia/Assets/Scripts/Controladores/ControladorPista.cs
@@ -6,6 +6,10 @@ public class ControladorPista : MonoBehaviour
 {
     public static ControladorPista controladorPista;
     public Vector3 goalRotation = new Vector3(0, 0, 0);
+    private float posToqueIniX, posToqueFinX;
+    private bool tocando = false;
+    Vector3 rotation = new Vector3(0, 0, 45.0f);
+    public float startTime = 0;
 
     void Start()
     {
@@ -15,6 +19,47 @@ public class ControladorPista : MonoBehaviour
     
     void Update()
     {
+#if UNITY_EDITOR
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            goalRotation -= rotation;
+            startTime= Time.time;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            goalRotation += rotation;
+            startTime= Time.time;
+        }
         
+#endif
+#if UNITY_ANDROID
+        if (Input.touchCount > 0)
+        {
+            Touch toque = Input.GetTouch(0);
+            switch (toque.phase)
+            {
+                case TouchPhase.Began:
+                    tocando = true;   
+                    posToqueIniX = Input.GetTouch(0).position.x;
+                    break;
+                case TouchPhase.Ended:  
+                tocando = false;
+                startTime= Time.time;
+                posToqueFinX =posToqueIniX;         
+                    posToqueFinX = Input.GetTouch(0).position.x;
+                    tocando = false;
+                    if (posToqueFinX - posToqueIniX>200)
+                    {
+                       goalRotation += rotation;
+                    }
+                    if (posToqueFinX - posToqueIniX<-200)
+                    {
+                        goalRotation -= rotation;
+                    }
+                    break;    
+            }
+        }
+
+#endif
     }
 }
